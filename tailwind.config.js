63a1dd4a7aa1d40306fd80bcaf7a24c234ecff0a
ASUS <abdulraheem3902@gmail.com>
2024-11-18 16:37:12 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Main HTML file
+    "./src/**/*.{html,js,jsx,ts,tsx}", // All files in src folder with specific extensions
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
